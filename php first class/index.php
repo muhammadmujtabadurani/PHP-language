dd6a15d4492afc0aaba0f059256b20180a9c32ce
muhammadmujtabadurani <145250058+muhammadmujtabadurani@users.noreply.github.com>
2024-03-07 20:48:50 +0500

    Add files via upload
    hello

---
+++ php first class/index.php
@@ -0,0 +1,58 @@
+<?php
+    $name = "hello world";
+    $age = 50;
+    $iscnic = true;
+    $marks = 80.65;
+
+
+    echo "$name";
+    echo "<br> $age";
+    echo "<br> $iscnic";
+    echo "<br> $marks";
+
+// Arthematic Operations//
+
+$a = 20;
+$b = 30;
+$c = $a + $b;
+$d = $a - $b;
+$e = $a * $b;
+$f = $a % $b;
+$g = $a / $b;
+echo "<br> The sum of $a + $b is:" .$c;
+echo "<br> The sum of $a - $b is:" .$d;
+echo "<br> The sum of $a * $b is:" .$e;
+echo "<br> The sum of $a % $b is:" .$f;
+echo "<br> The sum of $a / $b is:" .$g;
+
+// INCREMENT
+
+$apple = 70;
+//post Increment
+
+echo "<br>" .$apple++ ;//70
+echo "<br>" .$apple;
+// PRE INCREMENT
+echo "<br>" .++$apple;  //72
+
+// Task //
+//task
+
+$ya = 200;
+echo " <br> " .$ya;
+$ya -=5;
+echo " <br> " .$ya;
+$ya +=40;
+echo " <br> " .$ya;
+$ya -=126;
+echo " <br> " .$ya;
+$ya +=95;
+echo " <br> " .$ya;
+$ya -=124;
+echo " <br> " .$ya;
+$ya +=116;
+echo " <br> " .$ya;
+$ya +=104;
+echo " <br> " .$ya;
+
+?>
---
+++ php first class/loops.php
@@ -0,0 +1,18 @@
+<?php
+// LoopS //
+// For Loop //
+
+for($i=1; $i<=10; $i++){
+    echo "<br> This is for loop" .$i;
+}
+
+for($i=1; $i<=10; $i++){
+    echo "<br> 19 x $q = " .$q*19;
+}
+ 
+$tableNum = 64;
+for($w=1; $w<=10; $w++){
+    echo "<br> $tableNum x $w =" .$tableNum*$w;
+}
+
+?>
