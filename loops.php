4bc023c84ad8b8c41f7404af4c5f1d0e6fd9c8e8
muhammadmujtabadurani <145250058+muhammadmujtabadurani@users.noreply.github.com>
2024-03-12 17:50:47 +0500

    Add files via upload
    sdxeqs

---
+++ loops.php
@@ -0,0 +1,30 @@
+<?php
+// LoopS //
+// For Loop //
+
+for($i=1; $i<=10; $i++){
+    echo "<br> This is for loop" .$i;
+}
+
+for($i=1; $i<=10; $i++){
+    echo "<br> 19 x $q = " .$q*19;
+}
+ 
+$tableNum = 64;
+for($w=1; $w<=10; $w++){
+    echo "<br> $tableNum x $w =" .$tableNum*$w;
+}
+
+$number = 80;
+while($number<=85){
+    echo "<br> this is while loop $number";
+    $number++;
+}
+
+$apple = 35;
+do{
+    echo "<br> this is while loop $apple";
+    $apple++; 
+}
+while($apple<=35);
+?>
